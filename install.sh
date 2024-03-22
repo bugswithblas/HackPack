fb8cbd07da1c2220b17c6ce68d922005bf4d40bb
bgutowski <132657373+bgutowski@users.noreply.github.com>
2024-03-22 11:27:01 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+# PentestCheatsheets
+git clone https://github.com/bgutowski/HackPack.git /usr/local/bin
+cat <<EOT >> ~/.zshrc
+function _hackpack_completion() {
+  _files -W /usr/local/bin/HackPack -g 'hackpack-*'
+}
+
+function hackpack() {
+  cat /usr/local/bin/HackPack/$1
+}
+
+compdef _hackpack_completion hackpack
+EOT
